35f5f44ab80d09e89a6fd4c26ecce3c08c44b8b1
vladplesu <vladp1987@gmail.com>
2022-11-25 07:28:04 +0200

    add search and date picker filtering for table

--- src/components/DeputyLegislationInitiatives.tsx
+++ src/components/DeputyLegislationInitiatives.tsx
@@ -1,7 +1,10 @@
 import FindInPageIcon from "@mui/icons-material/FindInPage";
 import { IconButton } from "@mui/material";
-import { useMemo } from "react";
-import { useLegislationInitiativesByDeputyQuery } from "../queries";
+import { useCallback, useMemo, useState } from "react";
+import {
+  useCurrentLegislatureDetailsQuery,
+  useLegislationInitiativesByDeputyQuery,
+} from "../queries";
 import { legislationInitiativesTableColumns } from "../utils";
 import { Table } from "./Table";
 
@@ -12,8 +15,16 @@ type DeputyLegislationInitiativesProps = {
 export const DeputyLegislationInitiatives = ({
   did,
 }: DeputyLegislationInitiativesProps) => {
-  const { data } = useLegislationInitiativesByDeputyQuery(did);
-  console.log(data);
+  const [fromDate, setFromDate] = useState<Date>(new Date(Date.now()));
+  const [toDate, setToDate] = useState<Date>(new Date(Date.now()));
+  const { data, isFetching: isLoadingInitiatives } =
+    useLegislationInitiativesByDeputyQuery(did);
+  const { isFetching: isLoadingLegislatureDetails } =
+    useCurrentLegislatureDetailsQuery({
+      onSuccess: ({ legislature_from }) => {
+        setFromDate(new Date(legislature_from));
+      },
+    });
 
   const extendedColumns = useMemo(
     () => [
@@ -33,13 +44,45 @@ export const DeputyLegislationInitiatives = ({
     []
   );
 
+  const filteredInitiatives = useMemo(() => {
+    if (isLoadingInitiatives || isLoadingLegislatureDetails || !data) return [];
+
+    return data?.filter((initiative) => {
+      const regDate = new Date(initiative.data_reg);
+      return regDate >= fromDate && regDate <= toDate;
+    });
+  }, [
+    data,
+    fromDate,
+    isLoadingInitiatives,
+    isLoadingLegislatureDetails,
+    toDate,
+  ]);
+
+  const onFromDateChange = useCallback((date: Date | null) => {
+    if (!date) return;
+    setFromDate(date);
+  }, []);
+
+  const onToDateChange = useCallback((date: Date | null) => {
+    if (!date) return;
+    setToDate(date);
+  }, []);
+
   return (
     <Table
       columns={extendedColumns}
+      fromDate={fromDate}
       getRowId={(row) => row.docid}
       height={510}
+      isLoading={isLoadingInitiatives || isLoadingLegislatureDetails}
+      onFromDateChange={onFromDateChange}
+      onToDateChange={onToDateChange}
       pageSize={5}
-      rows={data ?? []}
+      rows={filteredInitiatives}
+      showSearch
+      showDatePickers
+      toDate={toDate}
     />
   );
 };
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,7 +6,11 @@ import {
   GridRowIdGetter,
   GridValidRowModel,
 } from "@mui/x-data-grid";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  DatePicker,
+  LocalizationProvider,
+  PickersDay,
+} from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { useMemo, useState } from "react";
 import { ro } from "date-fns/locale";
@@ -30,15 +34,28 @@ const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
   },
 }));
 
+const StyledPickersDay = styled(PickersDay<Date>)(({ theme }) => ({
+  "&.Mui-selected": {
+    backgroundColor: theme.palette.secondary.main,
+
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  },
+}));
+
 type TableProps = {
   columns: GridColumns<GridValidRowModel>;
   fromDate?: Date;
   getRowId: GridRowIdGetter<GridValidRowModel>;
   height: number | string;
+  isLoading?: boolean;
   onFromDateChange?: (date: Date | null) => void;
   onToDateChange?: (date: Date | null) => void;
   pageSize?: number;
   rows: GridValidRowModel[];
+  showDatePickers?: boolean;
+  showSearch?: boolean;
   toDate?: Date;
 };
 
@@ -50,10 +67,13 @@ export const Table = ({
   fromDate,
   getRowId,
   height,
+  isLoading,
   onFromDateChange,
   onToDateChange,
   pageSize = 3,
   rows,
+  showDatePickers,
+  showSearch,
   toDate,
 }: TableProps) => {
   const [search, setSearch] = useState("");
@@ -68,40 +88,56 @@ export const Table = ({
     [rows, search]
   );
 
-  console.log(filteredRows);
-
   return (
     <Stack gap={4}>
-      <Stack direction="row" gap={4}>
-        <TextField
-          placeholder="Caută..."
-          InputProps={{
-            startAdornment: <SearchIcon color="disabled" />,
-          }}
-          onChange={(event) => setSearch(event.target.value)}
-          value={search}
-        />
-        {onFromDateChange && (
-          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ro}>
-            <DatePicker
-              label="De la data"
-              onChange={onFromDateChange}
-              renderInput={(params) => <TextField {...params} />}
-              value={fromDate}
+      {(showSearch || showDatePickers) && (
+        <Stack direction="row" gap={4}>
+          {showSearch && (
+            <TextField
+              placeholder="Caută..."
+              InputProps={{
+                startAdornment: <SearchIcon color="disabled" />,
+              }}
+              onChange={(event) => setSearch(event.target.value)}
+              value={search}
             />
-          </LocalizationProvider>
-        )}
-        {onToDateChange && (
-          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ro}>
-            <DatePicker
-              label="Până la data"
-              onChange={onToDateChange}
-              renderInput={(params) => <TextField {...params} />}
-              value={toDate}
-            />
-          </LocalizationProvider>
-        )}
-      </Stack>
+          )}
+          {showDatePickers && onFromDateChange && (
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              adapterLocale={ro}
+            >
+              <DatePicker
+                label="De la data"
+                maxDate={toDate}
+                onChange={onFromDateChange}
+                renderDay={(date, selectedDays, pickersDayProps) => (
+                  <StyledPickersDay {...pickersDayProps} />
+                )}
+                renderInput={(params) => <TextField {...params} />}
+                value={fromDate}
+              />
+            </LocalizationProvider>
+          )}
+          {showDatePickers && onToDateChange && (
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              adapterLocale={ro}
+            >
+              <DatePicker
+                label="Până la data"
+                minDate={fromDate}
+                onChange={onToDateChange}
+                renderDay={(date, selectedDays, pickersDayProps) => (
+                  <StyledPickersDay {...pickersDayProps} />
+                )}
+                renderInput={(params) => <TextField {...params} />}
+                value={toDate}
+              />
+            </LocalizationProvider>
+          )}
+        </Stack>
+      )}
       <Box height={height}>
         <StyledDataGrid
           columns={columns}
@@ -111,9 +147,11 @@ export const Table = ({
           getRowId={getRowId}
           isCellEditable={() => false}
           isRowSelectable={() => false}
+          loading={isLoading}
           pageSize={pageSize}
           rowHeight={80}
           rows={filteredRows}
+          rowsPerPageOptions={[5]}
         />
       </Box>
     </Stack>
