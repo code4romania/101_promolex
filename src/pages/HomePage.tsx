980beeb61daba513a4ce0450b3d3513a857d8fa2
vladplesu <vladp1987@gmail.com>
2023-03-01 22:44:48 +0200

    add second banner

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
-import { Container, Stack } from '@mui/material';
+import { Button, Container, Stack, Typography } from '@mui/material';
+import secondBanner from '../assets/images/second_banner.png';
 import { HomeBanner, HomeNewsContainer } from '../components';
 
 export function HomePage() {
@@ -8,6 +9,26 @@ export function HomePage() {
         <HomeBanner />
 
         <HomeNewsContainer />
+
+        <Stack
+          alignItems='center'
+          height={360}
+          justifyContent='center'
+          gap={6}
+          sx={{
+            backgroundImage: `url(${secondBanner})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        >
+          <Typography fontWeight='bold' variant='h3'>
+            Cunoașteți deputații din Parlamentul Republicii Moldova
+          </Typography>
+          <Button color='secondary' variant='contained'>
+            Mergi la pagina Deputați
+          </Button>
+        </Stack>
       </Stack>
     </Container>
   );
